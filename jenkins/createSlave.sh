5ed5ec89df492b24c3f2e64dc21ef1aadb3c5e48
jpthiery <jpthiery@MacBook-Pro-de-Jean-Pascal.local>
2014-07-12 01:39:21 +0200

    begin agent bash script to create Node

---
+++ jenkins/createSlave.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+URL=$1
+NODE_NAME=$2
+
+if [[ -x jenkins-cli.jar ]] 
+then
+	echo "Already get cli"
+else
+	curl -o jenkins-cli.jar $URL/jnlpJars/jenkins-cli.jar
+	chmod +x jenkins-cli.jar
+fi
+	
+java -jar jenkins-cli.jar -s $URL create-node $NODE_NAME << EOF
+<slave>
+      <name>$NODE_NAME</name>
+      <description>Jenkins slave at your service</description>
+      <remoteFS>/</remoteFS>
+      <numExecutors>1</numExecutors>
+      <mode>NORMAL</mode>
+      <retentionStrategy class="hudson.slaves.RetentionStrategy$Always"/>
+      <launcher class="hudson.slaves.JNLPLauncher"/>
+      <label>slave</label>
+      <nodeProperties/>
+      <userId>anonymous</userId>
+</slave>
+EOF
